532d6dc73ce3288cb3418a3b10e773dd53056d8b
Rory Duncan <robust.rory@gmail.com>
2020-03-24 22:24:48 -0700

    Add room routes

---
+++ src/routes/room/create.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { onMount } from "svelte";
+
+  let isReady = false;
+  let CreateRoom = null;
+
+  onMount(async () => {
+    const module = await import("../../client_components/CreateRoom.svelte");
+    CreateRoom = module.default;
+    isReady = true;
+  })
+</script>
+
+{#if isReady}
+  <CreateRoom />
+{:else}
+  <p>
+    Loading...
+  </p>
+{/if}
