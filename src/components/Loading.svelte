9e3a2e8413598c44c8c7576ad9b5cfd3b20df947
Rory Duncan <robust.rory@gmail.com>
2020-03-29 14:35:30 -0700

    Move to support ssr component

--- src/components/Loading.svelte
+++ src/components/Loading.svelte
@@ -3,6 +3,16 @@
   export let message = "Loading...";
 
 </script>
+
+<style>
+  .loading {
+    background-color: #d2d2d2;
+    color: #000;
+    padding: 1rem 2rem;
+    width: auto;
+  }
+
+</style>
 {#if isLoading}
   <div class="loading">{message}</div>
 {:else}
