c2a28138f10d3dedc65a5656118b29723bc70c75
Rory Duncan <robust.rory@gmail.com>
2020-03-29 15:04:19 -0700

    scaffold out message service fns

--- src/services/messages.js
+++ src/services/messages.js
@@ -1,3 +1,22 @@
 import { database } from "./firebase.js";
 
 export const refName = "messages";
+
+/**
+ *
+ * @param {Function} callback
+ * @returns {Function} A function to call for unlistening to new messages
+ */
+export const listenForNewMessages = (id, callback) => {
+  const ref = database.ref(`${refName}/${id}`).limitToLast();
+  const thenable = ref.on("child_added", callback);
+  return () => ref.off("child_added", thenable);
+}
+
+export const getMessageHistory = async (id) => {
+
+}
+
+export const sendMessage = async (id, message) => {
+
+};
