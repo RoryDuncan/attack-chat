1f93a5c73c6243f67f49ee81332f9a8bcdaf8215
Rory Duncan <robust.rory@gmail.com>
2020-04-01 21:41:02 -0700

    Add firebase.config.js -- it's not a great pattern for ci

---
+++ src/config/firebase.config.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyCuBCkAo3W7_tnsYSw8uK7Eo358O3CHViE",
+  authDomain: "attack-chat.firebaseapp.com",
+  databaseURL: "https://attack-chat.firebaseio.com",
+  projectId: "attack-chat",
+  storageBucket: "attack-chat.appspot.com",
+  messagingSenderId: "1014263025801",
+  appId: "1:1014263025801:web:0511d46fb0c83cd1eaf658"
+}
