f8e0520f8ec9b8073907a48aa05be81b6681350c
Rory Duncan <robust.rory@gmail.com>
2020-04-04 20:11:14 -0700

    Add timestamps

--- src/client_components/Message.svelte
+++ src/client_components/Message.svelte
@@ -6,11 +6,15 @@
   export let timestamp;
   export let hasAttachment = false;
   export let isGroup = false;
+
+  const date = new Date(timestamp);
+  let time = date.toLocaleTimeString("en-us", { timeStyle: "short" });
 </script>
 
 <style>
   .message {
     padding: 0.5em 1em;
+    display: flex;
   }
 
   .author {
@@ -27,16 +31,35 @@
     padding-top: 0;
     margin-top: -0.25em;
   }
+
+  .timestamp {
+    font-size: 0.75em;
+    padding: 0.25em 0;
+    width: 5rem;
+    color: #ccc;
+  }
+
+  .hide-time .timestamp {
+    visibility: hidden;
+  }
 </style>
 
-<div id={id} class="message" class:is-group={isGroup} data-timestamp={timestamp}>
+<div
+  {id}
+  class="message"
+  class:hide-time={!hasAttachment && isGroup}
+  class:is-group={isGroup}
+  data-timestamp={timestamp}>
+  <div class="timestamp">{time}</div>
+  <div class="message-content">
+    {#if hasAttachment || !isGroup}
+      <div class="author" class:is-author={isAuthor}>{author}</div>
+    {/if}
 
-  {#if hasAttachment || !isGroup}
-    <div class="author" class:is-author={isAuthor}>{author}</div>
-  {/if}
-  <div class="text">{text}</div>
+    <div class="text">{text}</div>
 
-  {#if hasAttachment}
-    <slot />
-  {/if}
+    {#if hasAttachment}
+      <slot />
+    {/if}
+  </div>
 </div>
